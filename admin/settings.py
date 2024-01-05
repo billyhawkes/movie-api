fb84fab182d489e0e23e095835394ca3869df9a6
Billy Hawkes <billyhawkes02@gmail.com>
2024-01-05 09:52:57 -0500

    Added swagger / openapi

--- admin/settings.py
+++ admin/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "whitenoise.runserver_nostatic",
     "rest_framework",
+    "drf_spectacular",
     "base",
 ]
 
@@ -129,3 +130,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Movie API",
+    "DESCRIPTION": "Movie API made with Django and DRF",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
